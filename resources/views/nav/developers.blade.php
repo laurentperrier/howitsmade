2d0132c659b97e0b849f13259e01be195471d6fd
Laurent Perrier <laurent@thefoundation.com>
2017-08-17 00:41:31 -0400

    starting point

---
+++ resources/views/nav/developers.blade.php
@@ -0,0 +1,7 @@
+<div class="link-content">
+	@include('stripe.svg.documentation')
+	<div>
+		<h3 style="color: #b76ac4;">Developers</h3>
+		<p>Start integrating Stripe’s products and tools.</p>
+	</div>
+</div>
---
+++ resources/views/nav/products.blade.php
@@ -0,0 +1,21 @@
+<div class="link-content">
+	@include('stripe.svg.payments')
+	<div>
+		<h3 style="color: #6772e5;">Payments</h3>
+		<p>A complete commerce toolkit, built for developers.</p>
+	</div>
+</div>
+<div class="link-content">
+	@include('stripe.svg.subscriptions')
+	<div>
+		<h3 style="color: #24b47e;">Subscriptions</h3>
+		<p>The smart engine for recurring payments.</p>
+	</div>
+</div>
+<div class="link-content">
+	@include('stripe.svg.connect')
+	<div>
+		<h3 style="color: #3297d3">Connect</h3>
+		<p>Everything platforms need to get sellers paid.</p>
+	</div>
+</div>
